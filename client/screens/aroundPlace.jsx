3fc99a51802a91cdf6a422d543c391d782d62920
Sol Lee <dlthf316@naver.com>
2022-10-07 14:59:20 +0900

    장소_중간

--- client/screens/aroundPlace.jsx
+++ client/screens/aroundPlace.jsx
@@ -1,9 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Image, ImageBackground, Platform, Pressable, StyleSheet, TextInput, View } from "react-native";
+import { Entypo, AntDesign, Feather, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus, requestForegroundPermissionsAsync } from 'expo-location'
+import { useEffect, useState } from "react";
+import { createStaticMapURI, getReverseGeocordingURI } from "../Screens/util/map";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 function AroundPlaceScreen() {
+
+    async function setMapInformation(getLat, getLng) { // 맵과 주소를 불러오는 함수
+
+        setLoaded(true);
+
+        try {
+            const getMapURI = createStaticMapURI(getLat, getLng);
+
+            getReverseGeocordingURI(getLat, getLng)
+                .then((json) => {
+                    onGetPlaceLoca(getLat, getLng, json.results[0].formatted_address);
+                    setAddress(json.results[0].formatted_address);
+                }).catch(e => {
+                    console.log(e);
+                });
+
+                setMapURI(getMapURI);
+                setLoaded(false);
+            return;
+        } catch (e) {
+            console.log(e.message);
+            setLoaded(false);
+            return;
+        };
+    };
+
+
+
+
+
     return (  
-    <View>
+    <View style={styles.container}>
+        <View style={styles.containerInner}>
+
         <Text>around Place 페이지</Text>
+        </View>
     </View>
     );
 }
@@ -11,5 +49,16 @@ function AroundPlaceScreen() {
 export default AroundPlaceScreen;
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        width: "100%",
+    },
+    containerInner: {
+        flex: 1,
+        padding: 12,
+        margin: 12,
+        flexDirection: "column",
+        backgroundColor: "white",
+        borderRadius: 12,
+    },
 });
