e0288fe2a4a7ac327b5f29261ccce65d48268e32
Sol Lee <dlthf316@naver.com>
2022-10-07 16:56:00 +0900

    장소_지도끝

---
+++ client/components/placeInfoModal.jsx
@@ -0,0 +1,11 @@
+import { Modal } from "react-native";
+
+function PlaceInfoModal({visible}) {
+    return (
+        <Modal animationType="slide" visible={visible}>
+            
+        </Modal>
+    );
+}
+
+export default PlaceInfoModal;
